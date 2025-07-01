ea4223d1a423b0afc63e5301f4f4ae4534b80ade
takanori-matsushita <tknr2899@gmail.com>
2025-07-01 14:17:04 +0900

    [fix]baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://kids-web-coding.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/kids-web-coding/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
